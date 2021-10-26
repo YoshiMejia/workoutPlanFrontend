e9c8a3a3bb1d2e47dc82854f2c6b5c4a394b2cdf
Yoshi <yoshimejia00@gmail.com>
2021-10-26 16:27:37 -0400

    declared const variables for workouts and planners to add data to dom

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const port = `http://127.0.0.1:3000`;
+const workoutData = new WorkoutService(port); // create Workout object so that i can getWorkouts w parsed data (below)
+const plannerData = new PlannerService(port); // create Planner object so that i can getPlanners w parsed data (below)
+const workoutForm = document.getElementById("workout-form")
+const plannerSelect = document.getElementById("planner_select")
+const workoutName = document.getElementById("workout-name") 
+const workoutDesc = document.getElementById("workout-desc") 
+const workoutReps = document.getElementById("workout-reps") 
+const workoutSets = document.getElementById("workout-sets") 
+const plannerId = document.getElementById("planner_select") 
 
-const workout = new WorkoutService(port);
-workout.getWorkouts() // retreive parsed json data of all workouts from backend
+
+workoutForm.addEventListener('submit', handleSubmit) 
+
+function handleSubmit(e){
+    e.preventDefault()
+    workoutData.createWorkout()
+}
+
+plannerData.getPlanner()
+workoutData.getWorkouts() // retreive parsed json data of all workouts from backend
 //(6) [{…}, {…}, {…}, {…}, {…}, {…}]
 // 0: {id: 1, name: 'pushups', description: 'hands on floor, push', sets: 3, reps: 10, …}
 // 1: {id: 2, name: 'jumping jacks', description: 'jump and wave arms around', sets: 3, reps: 20, …}
@@ -11,3 +28,4 @@ workout.getWorkouts() // retreive parsed json data of all workouts from backend
 // 5: {id: 6, name: 'romanian deadlift', description: 'bend over and pick up barbel', sets: 4, reps: 8, …}
 // length: 6
 // [[Prototype]]: Array(0)
+
