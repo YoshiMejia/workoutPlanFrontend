f44e814727f812189fece37a5d156f9ab4159edb
Yoshi <yoshimejia00@gmail.com>
2021-10-26 16:28:30 -0400

    added functions for creating workouts and adding to dom

--- src/workout_service.js
+++ src/workout_service.js
@@ -6,6 +6,38 @@ class WorkoutService {
     getWorkouts(){
         fetch(this.port + `/workouts`)
         .then(resp => resp.json())
-        .then(data => console.log(data))
+        .then(data => {
+            for(const workout of data){
+                let newWorkout = new Workout(workout)
+                newWorkout.addToDOM()
+            }
+        })
     }
+    
+    createWorkout(){
+        const workoutObj = {
+            workout: {
+                name: workoutName.value,
+                description: workoutDesc.value,
+                sets: workoutSets.value,
+                reps: workoutReps.value,
+                planner_id: plannerId.value
+            }
+        }
+        const configObj = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "Accept": "application/json"
+            },
+            body: JSON.stringify(workoutObj)
+          }
+        fetch(this.port + `/workouts`, configObj)
+        .then(resp => resp.json())
+        .then(data => {
+            const w = new Workout(data)
+            w.addToDOM()
+        })
+    }
+
 }
