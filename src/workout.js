53237c51ed312df985e1fe69f33362faa806fe26
Yoshi <yoshimejia00@gmail.com>
2021-10-26 17:55:08 -0400

    added event listener for when user increases completed reps

--- src/workout.js
+++ src/workout.js
@@ -12,6 +12,7 @@ class Workout {
         this.planner_id = planner_id
         this.element = document.createElement('div')
         this.element.className = `workout-${name}-${id}` 
+        this.element.addEventListener('click', this.handleClick)
         Workout.all.push(this)
     }
 
@@ -23,9 +24,44 @@ class Workout {
             <li class ="sets"> Sets: ${this.sets}</li>
             <li class="reps"> Reps: ${this.reps}</li>
             <li class="compl"> Completed: ${this.completed}</li>
-        </div>
+            </div>
+            <button class="add_comp_reps">+</button>
         `
         return this.element
+    }
+    totalReps() {
+        return this.sets * this.reps
+    }
+    
+    completedWorkout(){
+        graveyard.innerHTML = `<h3>You crushed it, heres your workout graveyard!</h3>`
+        graveyard.appendChild(this.render())
+        const buffSponge = document.getElementById("buff_sponge")
+        setTimeout(function(){buffSponge.remove()}, 2000)
+    }
+   
+
+    increaseCompleted(){
+        const total = this.totalReps()
+        if (this.completed < total)    
+            {this.completed += this.reps
+                this.render()}
+            else if (this.completed >= total ) {
+               this.element.outerHTML = `<div id="buff_sponge"><img src="images/buff-spongebob.gif"></div>`
+                this.completedWorkout()
+            }
+    }
+ 
+
+    handleClick = (e) => {
+        if(e.target.innerText === "+"){
+            this.increaseCompleted()
+            this.updateDB()
+        }
+    }
+
+    updateDB(){
+        
     }
 
     addToDOM(){
