06dc42d85c6b0f4e46f835ece1ab5e0cf777a32f
Yoshi <yoshimejia00@gmail.com>
2021-10-27 11:57:22 -0400

    added comments for myself, as well as functions for creating planner objects

--- index.js
+++ index.js
@@ -1,21 +1,104 @@
 const port = `http://127.0.0.1:3000`;
 const workoutData = new WorkoutService(port); // create Workout object so that i can getWorkouts w parsed data (below)
 const plannerData = new PlannerService(port); // create Planner object so that i can getPlanners w parsed data (below)
+// const workoutButton = document.getElementById("new-workout")
+// const plannerButton = document.getElementById("new-planner")
+const newButtons = document.getElementById("new-buttons")
 const workoutForm = document.getElementById("workout-form")
-const plannerSelect = document.getElementById("planner_select")
-const workoutName = document.getElementById("workout-name") 
-const workoutDesc = document.getElementById("workout-desc") 
-const workoutReps = document.getElementById("workout-reps") 
-const workoutSets = document.getElementById("workout-sets") 
-const plannerId = document.getElementById("planner_select") 
-const graveyard = document.getElementById("graveyard") 
+const workoutName = document.getElementById("workout-name")
+const workoutDesc = document.getElementById("workout-desc")
+const workoutReps = document.getElementById("workout-reps")
+const workoutSets = document.getElementById("workout-sets")
+const plannerName = document.getElementById("planner-name")
+const graveyard = document.getElementById("graveyard")
 
 
-workoutForm.addEventListener('submit', handleSubmit) 
+workoutForm.addEventListener('submit', handleSubmit) // need to change to handle each separate submit button
+newButtons.addEventListener('click', handleClick)
 
+  
+// if Planner.all is empty, 
+function newWorkoutForm(){
+    workoutForm.innerHTML = `
+    <form id="workout-form">
+        <label for="workout-name">Workout Name:</label>
+        <input type="text" name="name" id="workout-name">
+        <br>
+        <label for="workout-desc">Workout Description:</label>
+        <input type="text" name="description" id="workout-desc">
+        <br>
+        <label for="workout-reps">How many reps?</label>
+        <input type="number" name="reps" id="workout-reps" min="1" max="30" value="1">
+        <br>
+        <label for="workout-sets">How many sets?</label>
+        <input type="number" name="sets" id="workout-sets"min="1" max="5" value="1">
+        <br>
+        <label for="planner_select">Select an existing planner:</label>
+        <select name="planner_id" id="planner_select">
+        <option hidden selected value> - select one - </option>
+        </select>
+        <br>
+        <input type="submit" value="Add Workout" id="workout-submit">    
+    </form>
+    `
+}
+// function newWorkoutForm(){ //--UNTOUCHED
+//     workoutForm.innerHTML = `
+//     <form id="workout-form">
+//         <label for="workout-name">Workout Name:</label>
+//         <input type="text" name="name" id="workout-name">
+//         <br>
+//         <label for="workout-desc">Workout Description:</label>
+//         <input type="text" name="description" id="workout-desc">
+//         <br>
+//         <label for="workout-reps">How many reps?</label>
+//         <input type="number" name="reps" id="workout-reps" min="1" max="30" value="1">
+//         <br>
+//         <label for="workout-sets">How many sets?</label>
+//         <input type="number" name="sets" id="workout-sets"min="1" max="5" value="1">
+//         <br>
+//         <label for="planner_select">Select an existing planner:</label>
+//         <select name="planner_id" id="planner_select">
+//         <option hidden selected value> - select one - </option>
+//         </select>
+//         <br>
+//         <input type="submit" value="Add Workout" id="workout-submit">    
+//     </form>
+//     `
+// }
+
+function newPlannerForm(){
+    workoutForm.innerHTML = `
+    <form id="planner-form">
+        <label for="workout-name">Planner Name:</label>
+        <input type="text" name="name" id="planner-name" placeholder="cardio day">
+        <br>
+        <input type="submit" value="Add Planner" id="planner-submit">    
+    </form>
+    `
+}
+
+function handleClick(e){
+    if (e.target.innerText === "Create new Workout"){
+        newWorkoutForm()
+    } else if (e.target.innerText === "Create new Planner"){
+        newPlannerForm()
+    }
+}
+
+// e.target is for the whole form. i have to separate the submit target to handle
+// each separate submit button. then i have to ensure i can create new planner objects
+//and that theyre persisting to the DB.
+// i should double check handleClick, make a function for the workout form to be displayed, make sure forms go away once theyre submitted
 function handleSubmit(e){
     e.preventDefault()
-    workoutData.createWorkout()
+    if(e.submitter.value === "Add Planner"){
+        console.log('add planner')
+        plannerData.createPlanner()
+    }else if(e.submitter.value === "Add Workout"){
+        console.log('add workout')
+        workoutData.createWorkout()
+    }
 }
 
 plannerData.getPlanner()
